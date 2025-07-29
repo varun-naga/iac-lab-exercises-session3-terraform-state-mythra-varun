a59e157d63b0739348cf47fdd4952f18dc1f6c70
varun-naga <varunmulugu2000@gmail.com>
2025-07-29 17:45:14 +0530

    Removed Dynamo DB dependency and enabled S3-native locking

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
     bucket         = "mythravarun-iac-lab-tfstate"
-    key            = "/backend_support/terraform.tfstate"
+    key            = "./backend_support/terraform.tfstate"
     region         = "eu-central-1"
-    dynamodb_table = "mythravarun-iac-lab-tfstate-locks"
     encrypt        = true
+    use_lockfile   = true
   }
 }
